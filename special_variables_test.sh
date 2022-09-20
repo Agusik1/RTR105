d7844a7d59544cd3a35ef5689329ce251264201a
Agusik1 <ak29424048@gmail.com>
2022-09-20 14:02:58 +0300

    Class_05

---
+++ special_variables_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "File Name: $0"
+echo "First Parameter : $1"
+echo "Second Parameter : $2"
+echo "Quoted Values: $@"
+echo "Quoted Values: $*"
+echo "Total Number of Parameters : $#2"
+
+# https://www.tutorialspoint.com/unix/unix-special-variables.htm#
+
+
