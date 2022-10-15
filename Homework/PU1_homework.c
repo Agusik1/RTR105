509b88a64cfd93bad15b010e0cf838195b249623
Agusik1 <112925785+Agusik1@users.noreply.github.com>
2022-10-15 23:37:50 +0300

    Add files via upload

---
+++ Homework/PU1_homework.c
@@ -0,0 +1,15 @@
+ // Homework PU1
+
+ #include<stdio.h>
+ 
+ int main()
+ {
+ unsigned long val1,val2,rezult;
+ printf("Pirmais skaitlis:");
+ scanf("%lu", &val1);
+ printf("Otrais skaitlis:");
+ scanf("%lu", &val2);
+ rezult=val1*val2;
+ printf("Reizinaajuma rezultaats: %lu \n", rezult);
+ return 0;
+ }
