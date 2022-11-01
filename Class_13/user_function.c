e4ffb03e624bcb2ee5b83c9b3f7fe7e8bc6801f1
Agusik1 <ak29424048@gmail.com>
2022-11-01 09:48:05 +0200

    Class_13

---
+++ Class_13/user_function.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+
+char dialogs(); // funkcijas deklarēšana
+char dialogs_i(int i_dialogs_arg);
+// char dialogs_i(int i_dialogs_arg); // argumenti var būt daudzi (secība ir svarīģa)
+// piemēram
+void test1(int a, int b, int c); // atslēgas vārds void nozīmē, ka return nebūs 
+void test2(int a, double b); // datu tips katram argumentam
+
+// funkcijas definēšana (var būt uzreiz definēšana bez deklarēšanas;
+//			 definēšana var būt ievietota pēc main apraksta;
+//			 deklarēšanai un definēšanai ir jābūt pirms f-jas izmantošanas)
+
+int main ()
+ {
+ char c_main_local;
+ c_main_local = 10;
+
+/*
+ printf("Cienījamais lietotāj, lūdzu, ievadi vienu naturālo skaitli: ");
+ scanf("%hhd",&c);
+ printf("Ir ievadīts skaitlis: %d\n",c);
+
+ /*{ ... }*/
+ /*
+ printf("Cienījamais lietotāj, lūdzu, ievadi vienu naturālo skaitli: ");
+ scanf("%hhd",&c);
+ printf("Ir ievadīts skaitlis: %d\n",c);
+ */
+
+
+ c_main_local = dialogs();
+ printf("Ir ievadīts skaitlis (izdruka no main funkc.): %d\n",c_main_local);
+ dialogs();
+
+ int i_main = 1;
+ dialogs_i(i_main);
+ i_main++;
+ dialogs_i(i_main);
+
+ return 0;
+ }
+
+// funkcijas definēšana (var būt uzreiz definēšana bez deklarēšanas;
+//                       definēšana var būt ievietota pēc main apraksta;
+//                       deklarēšanai un definēšanai ir jābūt pirms f-jas izmantošanas)
+// funkcijas datu tipam ir jāsaskan ar atgriežama (pie return) lieluma datu tipu
+
+ char dialogs()
+ {
+ char c_dialogs_local;
+ printf("Cienījamais lietotāj, lūdzu, ievadi vienu naturālo skaitli: ");
+ scanf("%hhd",&c_dialogs_local);
+ printf("Ir ievadīts skaitlis (izdruka no dialogs funkc.): %d\n",c_dialogs_local);
+ return c_dialogs_local;
+ }
+
+ char dialogs_i(int i_dialogs_arg)
+ {
+ char c_dialogs_local;
+ printf("\nFunkcija dialogs_i tiek izpildīta %d. reizi: \n",i_dialogs_arg);
+ printf("Cienījamais lietotāj, lūdzu, ievadi vienu naturālo skaitli: ");
+ scanf("%hhd",&c_dialogs_local);
+ printf("Ir ievadīts skaitlis (izdruka no dialogs funkc.): %d\n",c_dialogs_local);
+ return c_dialogs_local;
+ }
+
