445f77516d8650f3afe41a2a22dd9b156760399d
Agusik1 <112925785+Agusik1@users.noreply.github.com>
2022-12-23 22:29:23 +0100

    Update PU4_homework.c

--- Homework/PU4_homework.c
+++ Homework/PU4_homework.c
@@ -9,57 +9,95 @@
 #include<stdio.h>
  int main(){
 
- char i, c, n, e, fac1, fac2;
- int  h, j, k, l, fac3, fac4;
- long long r, t, y, u, fac5, fac6;
- char a;
+ char a, b, c, d, fac1, fac2;
+ int  e, f, g, h, fac3, fac4;
+ long long i, j, k, l, fac5, fac6;
+ char x;
 // Programmas datu tipa izvēle
   printf("Lūdzu izvēlieties datu tipa burtu - char(c), int(i), long long(l)! \n");
-  scanf("%c", &a);
+  scanf("%c", &x);
 
 //CHAR
- if(a == 0x63 || a == 0x43){
+ if(x == 0x63 || x == 0x43){
   printf("Izvēlētais datu tips char! \n");
   printf("Luudzu ievadiet vienu veselu dec skaitli: \n");
-  scanf("%hhd", &n);
+  scanf("%hhd", &c);
 
 
-  if(n == 0)
+  if(c == 0)
     printf("Faktoriāla rezultāts ir: 1 \n");
 
   else{
 
-    e = n - 1;
-    i = n;
-    c = 1;
+    d = c - 1;
+    a = c;
+    b = 1;
     fac1 = 1;
     fac2 = 1;
 
-    while(n / i !=n && c <= e){
+    while(c / a !=c && b <= d){
 
-      fac1 = fac1 * i;
-      i--;
+      fac1 = fac1 * a;
+      a--;
 
-      fac2 = fac2 * c;
-      c++;
+      fac2 = fac2 * b;
+      b++;
 
       if(fac1 <= fac2)
       break;
      }
 
 
-         if(fac1 / n == fac2)
+         if(fac1 / c == fac2)
           printf("Faktoriāla rezultāts ir: %d \n", fac1);
          else
-          printf("Ar izvēlēto datu tipu skaitlim !%d pareizi aprēķināt faktoriālu nav iespējams! \n", n);
+          printf("Ar izvēlēto datu tipu skaitlim !%d pareizi aprēķināt faktoriālu nav iespējams! \n", c);
    }
   }
 
 // INT
- if(a == 0x69 || a == 0x49){
+ if(x == 0x69 || x == 0x49){
   printf("Izvēlētais datu tips int! \n");
   printf("Luudzu ievadiet vienu veselu dec skaitli: \n");
-  scanf("%d", &k);
+  scanf("%d", &g);
+
+
+  if(g == 0)
+    printf("Faktoriāla rezultāts ir: 1 \n");
+
+  else{
+
+    h = g - 1;
+    e = g;
+    f = 1;
+    fac3 = 1;
+    fac4 = 1;
+
+    while(g / e !=g && f <= l){
+
+      fac3 = fac3 * e;
+      e--;
+
+      fac4 = fac4 * f;
+      f++;
+
+      if(fac3 <= fac4)
+      break;
+     }
+
+
+         if(fac3 / g == fac4)
+          printf("Faktoriāla rezultāts ir: %d \n", fac3);
+         else
+          printf("Ar izvēlēto datu tipu skaitlim !%d pareizi aprēķināt faktoriālu nav iespējams! \n", g);
+   }
+  }
+//LONG LONG
+
+ if(x == 0x6C || x == 0x4C){
+  printf("Izvēlētais datu tips long long! \n");
+  printf("Luudzu ievadiet vienu veselu dec skaitli: \n");
+  scanf("%lld", &k);
 
 
   if(k == 0)
@@ -68,66 +106,27 @@
   else{
 
     l = k - 1;
-    h = k;
+    i = k;
     j = 1;
-    fac3 = 1;
-    fac4 = 1;
-
-    while(k / h !=k && j <= l){
-
-      fac3 = fac3 * h;
-      h--;
-
-      fac4 = fac4 * j;
-      j++;
-
-      if(fac3 <= fac4)
-      break;
-     }
-
-
-         if(fac3 / k == fac4)
-          printf("Faktoriāla rezultāts ir: %d \n", fac3);
-         else
-          printf("Ar izvēlēto datu tipu skaitlim !%d pareizi aprēķināt faktoriālu nav iespējams! \n", k);
-   }
-  }
-//LONG LONG
-
- if(a == 0x6C || a == 0x4C){
-  printf("Izvēlētais datu tips long long! \n");
-  printf("Luudzu ievadiet vienu veselu dec skaitli: \n");
-  scanf("%lld", &y);
-
-
-  if(y == 0)
-    printf("Faktoriāla rezultāts ir: 1 \n");
-
-  else{
-
-    u = y - 1;
-    r = y;
-    t = 1;
     fac5 = 1;
     fac6 = 1;
 
-    while(y / r !=y && t <= u){
+    while(k / i !=k && j <= l){
 
-      fac5 = fac5 * r;
-      r--;
+      fac5 = fac5 * i;
+      i--;
 
-      fac6 = fac6 * t;
-      t++;
+      fac6 = fac6 * j;
+      j++;
 
       if(fac5 <= fac6)
       break;
      }
 
-
-         if(fac5 / y == fac6)
+         if(fac5 / k == fac6)
           printf("Faktoriāla rezultāts ir: %lld \n", fac5);
          else
-          printf("Ar izvēlēto datu tipu !%lld pareizi aprēķināt faktoriālu nav iespējams! \n", y);
+          printf("Ar izvēlēto datu tipu !%lld pareizi aprēķināt faktoriālu nav iespējams! \n", k);
    }
   }
  return 0;
