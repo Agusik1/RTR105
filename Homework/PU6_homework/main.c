8b6a2dc8535935886202a72abf877c56f3598e0c
Agusik1 <112925785+Agusik1@users.noreply.github.com>
2023-01-02 15:20:39 +0100

    Create main.c

---
+++ Homework/PU6_homework/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "user.h"
+
+int main(void) {
+print_hello_world(); 
+print_number(72); 
+
+int number = get_num(); 
+printf("Return number: %d\n", number);
+
+int plus = sum(7, 2); 
+printf("Summa: %d\n", plus);
+
+return 0;
+}
