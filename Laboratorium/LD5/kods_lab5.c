32bffd515cec08aabd1be7295ea58ab6e88932a9
Agusik1 <112925785+Agusik1@users.noreply.github.com>
2023-01-20 14:51:44 +0100

    Update kods_lab5.c

--- Laboratorium/LD5/kods_lab5.c
+++ Laboratorium/LD5/kods_lab5.c
@@ -4,4 +4,21 @@
 void main(){
   char string[51];
   char min[2];
+  char max[2];
+  char i;
+  int summa=0;
+  char skaits=0
+    
+ printf("Lūdzu ievadiet simbolu (teksta) rindu: ");
+ scanf("%s", &string);
+  min[0] = string[0];
+  min[1] = 0;
+  max[0] = string[0];
+  max[1] = 0;
+  
+ for(i=0; string[i]!='\0';i++)
+ {
+  summa=summa+string[i];
+ printf("%d. simbola (\"%c\") ASCII vērtība ir %d\n", i+1,string[i],string[i]);
+   
   
